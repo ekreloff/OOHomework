105656ed87cb0e6e95db988ba18e4adc7015efad
Ethan Kreloff <emkreloff@me.com>
2015-01-30 02:20:15 -0700

    Deleted compiled files

--- HW1/Media.java
+++
@@ -1,9 +0,0 @@
-/* Media.java
-* Modified by Ethan Kreloff on January 30th, 2015
-*/
-
-abstract class Media implements Comparable<Media>
-{
-    protected String title;
-	  public String getTitle()  { return title; }
-}
